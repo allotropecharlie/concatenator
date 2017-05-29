31f05ea828c834b1a44aeefe14270ad6e4faf9c5
Charlie Welsh <charlie@fourange.co>
2017-05-29 04:54:03 +0000

    Upload to GitHub

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+def main(args):
+    args = args.split(" ")
+    num = len(args)
+    content = []
+    os.chdir(args[0])
+
+    for filename in os.listdir():
+        if filename.endswith(".csv"):
+            f=open(filename)
+            fileread = f.read()
+            header=fileread.split("\n")
+            header=header[0]
+            fileread = fileread.replace(header+"\n","")
+            content.append(fileread)
+    content = ''.join(content)
+    final = header+content
+    outfile = open("/home/cwelsh/Documents/output.csv", "w")
+    outfile.write(final)
+    outfile.close()
+
+main("/home/cwelsh/Documents/BLE_Data")
